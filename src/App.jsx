9b035b987c8fd15006dc2f86cc9c299870687389
Rodrigo Ajnota <rodrigo.ajnota1@gmail.com>
2022-09-02 10:06:57 -0300

    corrijo direccion del ecommer

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <header className="fondo">
+      <div>
+        <div className="titulo">
+          <h1>MANN.CO</h1>
+          <div>
+            <div className="secciones">
+              <a href="...">Seccion 1</a>
+              <a href="...">Seccion 2</a>
+              <a href="...">Seccion 3</a>
+              <a href="...">Seccion 4</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default App;
